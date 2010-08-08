5534a36054e0278cbd6fecf72f4c26deebda6d6e
tokuhirom <tokuhirom@gmail.com>
2010-08-08 15:24:03 +0900

    initial import

---
+++ lib/Holsten/Util/DateTime.pm
@@ -0,0 +1,39 @@
+package Holsten::Util::DateTime;
+use strict;
+use warnings;
+use 5.00800;
+our $VERSION = '0.01';
+
+
+
+1;
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+Holsten::Util::DateTime -
+
+=head1 SYNOPSIS
+
+  use Holsten::Util::DateTime;
+
+=head1 DESCRIPTION
+
+Holsten::Util::DateTime is
+
+=head1 AUTHOR
+
+Tokuhiro Matsuno E<lt>tokuhirom AAJKLFJEF GMAIL COME<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+Copyright (C) Tokuhiro Matsuno
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
